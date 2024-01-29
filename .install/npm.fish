b93b017cff3cc89a0598b13e44b1b435e69f3a07
CofCat456 <a25690756@gmail.com>
2024-01-29 12:58:58 +0800

    fix(install): fix cz-conventional-changelog init

--- .install/npm.fish
+++ .install/npm.fish
@@ -5,6 +5,7 @@ npm i -g commitizen
 
 echo "Installing a changelog in commitizen"
 npm i -g cz-conventional-changelog
+echo '{ "path": "cz-conventional-changelog" }' >~/.czrc
 
 echo "Installing a typescript"
 npm i -g typescript
