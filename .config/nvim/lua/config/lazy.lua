27a99c641862f6930068fc3356d0e96aa9c835e0
CofCat456 <a25690756@gmail.com>
2024-03-17 00:28:49 +0800

    perf(neovim): to reduce memory usage
    refer to: https://x.com/inkdrop_app/status/1768520387514782002?s=20

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -38,7 +38,7 @@ require("lazy").setup({
 		-- { import = "lazyvim.plugins.extras.dap.core" },
 		-- { import = "lazyvim.plugins.extras.vscode" },
 		{ import = "lazyvim.plugins.extras.editor.navic" },
-		{ import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+		-- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 		-- { import = "lazyvim.plugins.extras.test.core" },
 		-- { import = "lazyvim.plugins.extras.coding.yanky" },
--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -8,4 +8,13 @@ return {
 		"nvim-pack/nvim-spectre",
 		enabled = false,
 	},
+
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		enabled = false,
+	},
+	{
+		"echasnovski/mini.indentscope",
+		enabled = false,
+	},
 }
