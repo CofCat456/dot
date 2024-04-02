a95ada509ecfff8af2f5f1ff133d79cb6a620b61
CofCat456 <a25690756@gmail.com>
2024-04-02 16:22:15 +0800

    feat(neovim): add garbage-day.nvim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -220,4 +220,13 @@ return {
 			},
 		},
 	},
+
+	{
+		"zeioth/garbage-day.nvim",
+		dependencies = "neovim/nvim-lspconfig",
+		event = "VeryLazy",
+		opts = {
+			notifications = true,
+		},
+	},
 }
